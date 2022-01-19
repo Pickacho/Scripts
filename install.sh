7822f0f51b1ec8fdb6f4007ea9197a6c2a301d9b
Pickacho <40912416+Pickacho@users.noreply.github.com>
2022-01-19 23:32:12 +0200

    Instllk8s

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo 157953 > sudo su
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y\
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io ansible
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
